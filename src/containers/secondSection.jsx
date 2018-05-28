16dd1f0e5c3b0d6e4eae8deff98a53ac44bf1831
Helene Rignér <hvs@qlik.com>
2018-05-28 10:50:52 +0200

    Adding life expectancy rate and urbanization rate calculations (#11)

--- src/containers/secondSection.jsx
+++ src/containers/secondSection.jsx
@@ -8,7 +8,9 @@ import Heart from "../components/heart";
 import {
   africanCountries,
   avgLifeExpFemale,
-  avgLifeExpMale
+  avgLifeExpMale,
+  lifeExpRate,
+  urbanizationRate
 } from "../definitions";
 import "./section.css";
 import "./secondSection.css";
@@ -16,20 +18,22 @@ import "./secondSection.css";
 class SecondSection extends React.Component {
   constructor(...args) {
     super(...args);
-    this.state = { loaded: false };
+    this.state = { loaded: false, selectedCountry: "" };
   }
 
   componentDidMount() {
     this.createModel();
   }
 
-  async updateAvgLifeExp() {
-    const avgLifeExpFemaleLayout = await this.state.avgLifeExpFemaleModel.getLayout();
-    const avgLifeExpFemaleNbr = this.getNbr(avgLifeExpFemaleLayout);
-    const avgLifeExpMaleLayout = await this.state.avgLifeExpMaleModel.getLayout();
-    const avgLifeExpMaleNbr = this.getNbr(avgLifeExpMaleLayout);
-    this.setState({ avgLifeExpFemaleNbr, avgLifeExpMaleNbr });
-  }
+  getNbr = layout => {
+    const nbr = layout.qHyperCube.qGrandTotalRow[0].qNum;
+    if (nbr !== "NaN") {
+      return Math.round(nbr);
+    }
+    return "-";
+  };
+
+  getText = layout => layout.qHyperCube.qGrandTotalRow[0].qText;
 
   async createModel() {
     try {
@@ -42,37 +46,78 @@ class SecondSection extends React.Component {
       const avgLifeExpMaleModel = await this.props.app.createSessionObject(
         avgLifeExpMale
       );
+      const lifeExpRateModel = await this.props.app.createSessionObject(
+        lifeExpRate
+      );
+      const urbRateModel = await this.props.app.createSessionObject(
+        urbanizationRate
+      );
+
       const africanCountriesLayout = await africanCountriesModel.getLayout();
       const avgLifeExpFemaleLayout = await avgLifeExpFemaleModel.getLayout();
       const avgLifeExpMaleLayout = await avgLifeExpMaleModel.getLayout();
+      const lifeExpRateLayout = await lifeExpRateModel.getLayout();
+      const urbRateLayout = await urbRateModel.getLayout();
 
       const avgLifeExpFemaleNbr = this.getNbr(avgLifeExpFemaleLayout);
       const avgLifeExpMaleNbr = this.getNbr(avgLifeExpMaleLayout);
+      const lifeExpRateNbr = this.getText(lifeExpRateLayout);
+      const urbRateNbr = this.getText(urbRateLayout);
 
       this.setState({
         africanCountriesModel,
         avgLifeExpFemaleModel,
         avgLifeExpMaleModel,
+        lifeExpRateModel,
+        urbRateModel,
         africanCountriesLayout,
         avgLifeExpFemaleNbr,
         avgLifeExpMaleNbr,
+        lifeExpRateNbr,
+        urbRateNbr,
         loaded: true
       });
 
-      avgLifeExpFemaleModel.on("changed", () => this.updateAvgLifeExp());
-      avgLifeExpMaleModel.on("changed", () => this.updateAvgLifeExp());
+      avgLifeExpFemaleModel.on("changed", () => this.updateAvgLifeExpFemale());
+      avgLifeExpMaleModel.on("changed", () => this.updateAvgLifeExpMale());
+      lifeExpRateModel.on("changed", () => this.updateLifeExpRate());
+      urbRateModel.on("changed", () => this.updateUrbRate());
     } catch (error) {
       // console.log(error);
     }
   }
 
-  getNbr = layout => {
-    const nbr = layout.qHyperCube.qGrandTotalRow[0].qNum;
-    if (nbr !== "NaN") {
-      return Math.round(nbr);
-    }
-    return "-";
+  selectedCountry = country => {
+    this.setState({ selectedCountry: country });
   };
+
+  async updateAvgLifeExpFemale() {
+    const avgLifeExpFemaleLayout = await this.state.avgLifeExpFemaleModel.getLayout();
+    const avgLifeExpFemaleNbr = this.getNbr(avgLifeExpFemaleLayout);
+
+    this.setState({ avgLifeExpFemaleNbr });
+  }
+
+  async updateAvgLifeExpMale() {
+    const avgLifeExpMaleLayout = await this.state.avgLifeExpMaleModel.getLayout();
+    const avgLifeExpMaleNbr = this.getNbr(avgLifeExpMaleLayout);
+
+    this.setState({ avgLifeExpMaleNbr });
+  }
+
+  async updateLifeExpRate() {
+    const lifeExpRateLayout = await this.state.lifeExpRateModel.getLayout();
+    const lifeExpRateNbr = this.getText(lifeExpRateLayout);
+
+    this.setState({ lifeExpRateNbr });
+  }
+
+  async updateUrbRate() {
+    const urbRateLayout = await this.state.urbRateModel.getLayout();
+    const urbRateNbr = this.getText(urbRateLayout);
+
+    this.setState({ urbRateNbr });
+  }
 
   render() {
     if (!this.state.loaded) {
@@ -95,6 +140,7 @@ class SecondSection extends React.Component {
           <Filterbox
             layout={this.state.africanCountriesLayout}
             model={this.state.africanCountriesModel}
+            selectedValueCallback={country => this.selectedCountry(country)}
           />
           <Heart />
         </div>
@@ -113,11 +159,18 @@ class SecondSection extends React.Component {
               <div className="didyouknow" />
               <div className="infotext">
                 <div>
-                  <b>Egypt</b> life expectancy in <b>2016</b> has risen with{" "}
-                  <b>45%</b> compared to <b>1960</b>.
+                  <b>
+                    {this.state.selectedCountry
+                      ? this.state.selectedCountry
+                      : "African"}
+                  </b>{" "}
+                  life expectancy in <b>{this.props.selectedYear}</b> has risen
+                  with <b>{this.state.lifeExpRateNbr}</b> compared to{" "}
+                  <b>1960</b>.
                   <br />
                   <br />
-                  While urbanization compared to <b>1960</b> is up <b>200%</b>.
+                  While urbanization compared to <b>1960</b> is up{" "}
+                  <b>{this.state.urbRateNbr}</b>.
                 </div>
               </div>
             </div>
