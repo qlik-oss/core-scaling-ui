fd3b9b37764bc25273523545467464ac4fd53df0
Henrik Carlioth <hca@qlik.com>
2018-05-29 15:05:12 +0200

    Enviroment variable for backend adress

--- src/enigma-config.js
+++ src/enigma-config.js
@@ -1,8 +1,10 @@
 import qixSchema from 'enigma.js/schemas/12.20.0.json';
 
+const backendAdress = process.env.NODE_ENV === 'production' ? `${process.env.BACKEND}/app/doc/Shared-Africa-Urbanization.qvf` : 'localhost:9076';
+
 const config = {
   schema: qixSchema,
-  url: `${location.protocol.replace('http', 'ws')}//scalingbackend.qlikcore.com/app/doc/Shared-Africa-Urbanization.qvf`,
+  url: `${location.protocol.replace('http', 'ws')}${backendAdress}`,
 };
 
 
