ddbdc2c8aefa1b93421673ae872f61b97ea11264
Ann-Louise Andersson <add@qlik.com>
2018-06-01 13:16:01 +0200

    Add Qlik Core logo

--- src/containers/header.jsx
+++ src/containers/header.jsx
@@ -3,10 +3,19 @@ import PropTypes from "prop-types";
 import "./header.css";
 import houseImg from "../resources/house.svg";
 import heartImg from "../resources/heart.svg";
+import qlikCoreImg from "../resources/QlikCoreLogo.svg";
 
 export default function Header(props) {
   return (
     <div className="header">
+      <img
+        className="coreLogo"
+        src={qlikCoreImg}
+        alt="Qlik Core Logo"
+        onClick={() => {
+          window.open("https://qlikcore.com");
+        }}
+      />
       <button
         title="Urbanization"
         className={`headerButton ${
@@ -16,11 +25,7 @@ export default function Header(props) {
           props.onClick("urbanization");
         }}
       >
-        <img
-          className="buttonImage"
-          src={houseImg}
-          alt="Urbanization button"
-        />
+        <img className="buttonImage" src={houseImg} alt="Urbanization button" />
       </button>
       <button
         title="Life Expectancy"
