fd76170e3903167790903baeefc25fcf1d95c799
Henrik Carlioth <carlioth@users.noreply.github.com>
2019-02-05 11:02:25 +0100

    Updated appid for persistancemode 2 (#196)

--- src/enigma-config.js
+++ src/enigma-config.js
@@ -1,6 +1,6 @@
 import qixSchema from 'enigma.js/schemas/12.20.0.json';
 
-const backendAdress = process.env.NODE_ENV === 'production' ? `${process.env.BACKEND}/app/doc/5f5f03fe-00f3-4a4b-ac52-db32152a7e44` : 'localhost:9176';
+const backendAdress = process.env.NODE_ENV === 'production' ? `${process.env.BACKEND}/app/doc/739db838-dd28-4078-8715-ee9cfcc06c29` : 'localhost:9176';
 
 const enigmaConfig = {
   schema: qixSchema,
